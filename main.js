2882de52c350633f6ecbd7a2bab25ac8d0d95bce
Madhu22 <madhuxsharma@gmail.com>
2023-10-01 16:39:12 -0700

    Changes

--- main.js
+++ main.js
@@ -44,53 +44,61 @@ function run(){
 
   const jobDescription = document.getElementById("jobDescription");
 
+  document.getElementById("btn2").addEventListener('click', () => {
+    jobDescription.innerHTML = `<h6>Software Dev Engineer Intern @ Amazon</h6>
+    <p>June 2022 - September 2022</p>
+    <br><br>
+    <ul>
+      <li>Accelerated promotion approval speed by 2400% by designing and developing features on an internal application to aid product leads across Amazon in planning and executing product discount promotions</li>
+      <li>Took ownership of project through working directly with the customers and designers to identify areas for improvement, leading to an overall improvement of user experience in the final product</li>
+      <li>Improved API logging to 100% coverage in methods to help developers better debug errors during development</li>
+    </ul>`
+  });
+
   document.getElementById("btn1").addEventListener('click', () => {
     jobDescription.innerHTML = `<h6>Software Dev Engineer Intern @ Amazon</h6>
-    <h7>June 2022 - September 2022</h7>
+    <p>June 2023 - September 2023</p>
     <br><br>
     <ul>
-      <li>Designed and developed features on an internal application to help different product leads across Amazon plan and execute promotions</li>
-      <li>Improved API logging to assist developers in understanding errors </li>
-      <li>Wrote unit tests and integration tests to improve coverage</li>
-    </ul>`
-  });
-
-  document.getElementById("btn2").addEventListener('click', () => {
-    jobDescription.innerHTML = `<h6>Full Stack Developer @ Commit the Change</h6>
-    <h7>October 2021 - Present</h7>
-    <br><br>
-    <ul>
-      <li>Currently working with Abound Food Care to reduce food waste and insecurity by creating a digital registration system to better connect their volunteers and engage them in organizational events using React, PostgreSQL, Express, and Node.js</li>
-      <li>Assess problems, develop, and maintain high quality software for non-profit organizations to help increase productivity and save time</li>
-      <li>Aspiring to become a Technical Lead for a non-profit organization project in the upcoming year</li>
-    </ul>`
-  });
-
-  document.getElementById("btn3").addEventListener('click', () => {
-    jobDescription.innerHTML = `<h6>Junior Software Developer @ Delta System</h6>
-    <h7>June 2020 - August 2021</h7>
-    <br><br>
-    <ul>
-      <li>Architected and programmed a prototype ticketing system to manage customer service requests for the 
-      human resources department which resulted in an increase in productivity</li>
-      <li>Performed responsibilities assigned by my manager for organizational purposes</li>
-      <li>Helped market job openings at career fairs</li>
+      <li>Replacing a two-step process of adding content onto FireTV Channels involving product managers and developers with a single step, user-friendly service; saving an estimated 500 hours of time over the next 5 years with this portal</li>
+      <li>Architected the full long-term solution involving decision-making about backend, frontend, and midtier service selections based on comprehensive pros and cons analysis; will enable future projects to be extended in the future</li>
     </ul>`
   });
 
   document.getElementById("btn4").addEventListener('click', () => {
-    jobDescription.innerHTML = `<h6>Teacher @ iCode</h6>
-    <h7>February 2019 - August 2021</h7>
+    jobDescription.innerHTML = `<h6>Junior Software Developer @ Delta System</h6>
+    <p>June 2020 - August 2021</p>
     <br><br>
     <ul>
-      <li>Bridged technical concepts of Computer Science to non-technical children ages 5-12 in both weekly classes and summer camps.</li>
-      <li>Helped improve children's ability to think critically and apply their skills to solve real world problems. </li>
-      <li>Marketed class openings to parents leading to an increase in registered students</li>
-      <li>Taught classes in areas such as Robotics, HTML, CSS, Javascript, Scratch, and basic game development</li>
+      <li>Innovated a prototype ticketing system for the Human Resources department, resulting in a significant productivity boost in handling customer service requests for hundreds of employees and clients </li>
+      <li>Employed Excel for detailed analysis and evaluation of numerous resumes during recruitment events consistingof hundreds of interested applicants, facilitating efficient identification and tracking of suitable candidates</li>
     </ul>`
   });
 
+  document.getElementById("btn3").addEventListener('click', () => {
+    jobDescription.innerHTML = `<h6>Technical Lead @ Commit the Change</h6>
+    <p>October 2021 - Present</p>
+    <br><br>
+    <ul>
+      <li>Migrated Cell Dogs Google Sheets spreadsheet to a website for employees to easily create, update, and find information regarding rehabilitation programs for over 300 dogs and 15 facilities across Orange County</li>
+      <li>Steered strategic planning and execution of a year-long project, effectively managing a team of 12 people</li>
+      <li>Implemented a digital registration system for Abound Food Care, enhancing engagement and participation for over 100 volunteers in various events through</li>
+    </ul>`
+  });
+
+  document.getElementById('bonsai').addEventListener('click', function() {
+    window.location.href = "https://www.youtube.com/watch?v=rN3m2eAzhJ4&ab_channel=GROUPAntrepreneurCenter";
+  });
+
+  document.getElementById('retnoscan').addEventListener('click', function() {
+    window.location.href = "https://d2k.rice.edu/news/congratulations-hackrice-x-2020-winners";
+  });
+
+  document.getElementById('faceanalyzer').addEventListener('click', function() {
+    window.location.href = "https://github.com/KanishkGar/FaceAnalyzer";
+  });
   
+
 
 }
 
